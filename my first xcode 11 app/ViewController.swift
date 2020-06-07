aad248a1a1615fedb52026a8cfb26bbe120a68d1
Annamaria <annamaria.ferrante1956@gmail.com>
2020-06-07 13:12:10 +0200

    did you clicked

--- my first xcode 11 app/ViewController.swift
+++ my first xcode 11 app/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnHello: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnDidClicked(_ sender: UIButton) {
+        print("Button Clicked")
+        
+        btnHello.setTitle("you clicked me", for: .normal)
+    }
+    
 }
 
